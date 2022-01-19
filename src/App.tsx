5b8a78f45a52e4e43da1b1d5e9fd14691a8edf7c
Moises Guilherme <moisescomp@gmail.com>
2022-01-19 16:04:27 -0300

    Changing theme to dark mode using context-api

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
 
   
   return (
-    <div className="App">
+    <div className={`App ${theme}`} >
       <ThemeContext.Provider value={{theme, setTheme }}>
         <Routes />  
       </ThemeContext.Provider>
--- src/Home.tsx
+++ src/Home.tsx
@@ -1,9 +1,16 @@
+import { useContext } from "react";
+import { ThemeContext } from "./ThemeContext";
+
  const Home = () => {
+    
+    const { theme, setTheme } = useContext(ThemeContext)
+
     return  (
      <div>
          <h1>Aqui é tela home </h1>
-         <button onClick={() => null} >DARK MODE</button>
-        <button onClick={() => null} >LIGHT MODE</button>  
+         <h4>Valor do theme: theme</h4>
+         <button onClick={() => setTheme('DARK')} >DARK MODE</button>
+         <button onClick={() => setTheme('LIGHT')} >LIGHT MODE</button>  
      </div>
     );
 } 
